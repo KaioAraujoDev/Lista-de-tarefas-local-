6328a809d68614a2027585fb17ac97b39cd10222
KaioDev08 <Kaioaraujo6190@outlook.com>
2022-07-18 16:39:24 -0300

    adicionando função de criação de elementos da lista

--- js/animations.js
+++ js/animations.js
@@ -5,13 +5,13 @@ const modalList = document.querySelector('.boxList');
 const body = document.querySelector('.boxMain');
 
 
-buttonOpenTask.addEventListener('click',()=>{
+buttonOpenTask.addEventListener('click', () => {
     body.style.marginRight = "20vw";
     modalList.classList.remove('hideModal');
     modalList.classList.add('showModal');
 });
 
-buttonHideTask.addEventListener('click',()=>{
+buttonHideTask.addEventListener('click', () => {
     modalList.classList.remove('showModal');
     modalList.classList.add('hideModal');
     body.style.marginRight = "0";
@@ -19,13 +19,17 @@ buttonHideTask.addEventListener('click',()=>{
 
 //Controle de configurações tela de lista 
 
-const configItem =  document.querySelector('[data-buttonSettings]')
-const listEdit = document.querySelector('[data-editItem]')
-configItem.addEventListener('click',()=>{
-    if(listEdit.style.display == 'block'){
-        listEdit.style.display = 'none';
-    }else{
-        listEdit.style.display = 'block';
-    }
+console.log(lista)
+if (lista.children.length >= 1) {
     
-})
+    const configItem = document.querySelector('[data-buttonSettings]')
+    const listEdit = document.querySelector('[data-editItem]')
+    configItem.addEventListener('click', () => {
+        if (listEdit.style.display == 'block') {
+            listEdit.style.display = 'none';
+        } else {
+            listEdit.style.display = 'block';
+        }
+
+    })
+}
--- js/main.js
+++ js/main.js
@@ -1,9 +1,57 @@
-const form = document.querySelector('#form');
+const form = document.getElementById('form');
+const lista = document.getElementById('lista');
 
-form.addEventListener('submit',(evento) =>{
+
+form.addEventListener('submit', (evento) => {
     evento.preventDefault();//Impedir a propagação do evento
 
-    console.log(evento.target.elements['tarefa'].value);
-    console.log(evento.target.elements['descricao'].value);
-    console.log(evento.target.elements['data'].value);
+
+    createItem(evento.target.elements['tarefa'].value, evento.target.elements['descricao'].value, evento.target.elements['data'].value);
 })
+
+function createItem(tarefa, descricao, data) {
+    console.log(tarefa, descricao, data);
+
+    const novoItem = document.createElement('li');
+    novoItem.classList.add('item');
+
+    novoItem.innerHTML = `
+    <div class='header'>
+        <h2 class='titleItemList'>${tarefa}</h2>
+        <div class="settings">
+            <button class="buttonSettings" data-buttonSettings><i><img class="iconSettings"
+            src="assets/three-dots.svg" alt=""></i></button>
+            <nav class="editItem" data-editItem>
+                <ul>
+                    <li>
+                        <button class="buttonSettings">
+                            <i><img class="iconSettings" src="assets/pencil-square.svg" alt="Editar"></i>
+                            Editar tarefa
+                        </button>
+
+                    </li>
+                    <li>
+                        <button class="buttonSettings">
+                            <i><img class="iconSettings" src="assets/trash-fill.svg" alt="Remover"></i>
+                            Remover tarefa
+                        </button>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    </div>
+        <div class="stateList">
+            <p>Em progresso</p>
+        </div>
+        <div class="boxItem">
+            <p class="contentItem">
+                ${descricao}
+            </p>
+        </div>
+    <p class="dataList">Prazo: ${data}</p>
+    `;
+
+    lista.appendChild(novoItem);
+
+    console.log(lista)
+}
